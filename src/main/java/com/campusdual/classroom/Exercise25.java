693fcf4cfa97c727acee7bd8a351e17134bb871e
Velasco <quarantine.sleep480@passinbox.com>
2024-09-25 22:53:15 +0200

    a

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -3,18 +3,25 @@ package com.campusdual.classroom;
 import java.util.Stack;
 
 public class Exercise25 {
-    public static Stack<String> createStack() {
 
+    // Method to create and return a stack with the specified elements
+    public static Stack<String> createStack() {
+        Stack<String> stack = new Stack<>();
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+        return stack;
     }
 
+    // Method to print and empty the stack
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
 
     }
-
-
-
 }
